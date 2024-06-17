cef0398919b9493b15f6a5e79facf1194b5d0815
Duan Cleypaul <duancleypaul@Duans-MacBook-Pro.local>
2024-06-17 11:07:38 -0300

    feat: built feature NOME and use it for similarity

--- cnae_app.py
+++ cnae_app.py
@@ -15,13 +15,13 @@ st.set_page_config(
 #-------------------------------- CACHED FUNCTIONS
 @st.cache_resource(show_spinner=False)
 def get_model():
-    return SentenceTransformer('distiluse-base-multilingual-cased-v1', truncate_dim=64)
+    return SentenceTransformer('distiluse-base-multilingual-cased-v1')
 
 @st.cache_data(show_spinner="Buscando recomendações...")
-def get_recommendations(df, cnae_cliente, razao_social_cliente):
+def get_recommendations(df, cnae_cliente, nome_cliente):
     model = get_model()
-    razao_social_todos = razao_social_cliente + df.loc[df['CNAE FISCAL PRINCIPAL']==cnae_cliente, 'RAZÃO SOCIAL'].values.tolist()
-    client_embeddings  = model.encode(razao_social_todos)
+    nome_todos = nome_cliente + df.loc[df['CNAE FISCAL PRINCIPAL']==cnae_cliente, 'NOME'].values.tolist()
+    client_embeddings  = model.encode(nome_todos)
     df_rec             = df[(df['CNAE FISCAL PRINCIPAL']==cnae_cliente) & (df['CNPJ']!=cnpj_cliente)].copy()
     df_rec['SIMILARIDADE'] = cosine_similarity(client_embeddings)[1:,0]
     df_rec = df_rec.sort_values(by='SIMILARIDADE', ascending=False)
@@ -29,7 +29,7 @@ def get_recommendations(df, cnae_cliente, razao_social_cliente):
 
 @st.cache_data
 def read_data():
-    return pd.read_csv('pre-processed-data-100k.csv').drop(columns='ID'), pd.read_csv('preprocessed-clientes.csv')
+    return pd.read_csv('pre-processed-data-100k.csv'), pd.read_csv('preprocessed-clientes.csv')
 
 #-------------------------------- DATA LOAD
 df, df_clientes = read_data()
@@ -41,10 +41,11 @@ with st.sidebar:
     cnpj_cliente = st.selectbox("CNPJ Base:", df_clientes['CNPJ'].sort_values().unique(), index=74) 
     df_cliente = df_clientes[df_clientes['CNPJ']==cnpj_cliente].copy()
     cnae_cliente = df_cliente['CNAE FISCAL PRINCIPAL'].values[0]
-    razao_social_cliente = df_clientes.loc[df_clientes['CNPJ']==cnpj_cliente,'RAZÃO SOCIAL'].values.tolist()
+    nome_cliente = df_clientes.loc[df_clientes['CNPJ']==cnpj_cliente,'NOME'].values.tolist()
 
 st.write("# Info Cliente:")
 st.write(f"**CNPJ Base:** {cnpj_cliente}")
+st.write(f"**Nome Fantasia:** {df_cliente['NOME FANTASIA'].values[0]}")
 st.write(f"**Razão Social:** {df_cliente['RAZÃO SOCIAL'].values[0]}")
 st.write(f"**CNAE principal:** {cnae_cliente}")
 st.write(f"**CNAE secundária:** {df_cliente['CNAE FISCAL SECUNDÁRIA'].values[0]}")
@@ -53,10 +54,10 @@ st.divider()
 
 st.write("# Recomendações:")
 
-df_rec = get_recommendations(df, cnae_cliente, razao_social_cliente)
+df_rec = get_recommendations(df, cnae_cliente, nome_cliente)
 
 st.data_editor(
-    df_rec.head(top_k).reset_index(drop=True),
+    df_rec.drop(columns='NOME').head(top_k).reset_index(drop=True),
     column_config={
         "CNPJ": st.column_config.NumberColumn(
             help="8 primeiros dígitos do CNPJ",
